44d4da66e894a21605a9c92b47a9bf98659cc728
Vitor <vitorcsharp@hotmail.com>
2022-07-28 21:37:50 -0300

    passando state no contexto

--- src/app/shared/contexts/UsuarioLogado.tsx
+++ src/app/shared/contexts/UsuarioLogado.tsx
@@ -1,6 +1,6 @@
 //componente para usar o nome do usuario em mais de uma tela
 
-import { createContext, useCallback } from "react";
+import { createContext, useCallback, useEffect, useState } from "react";
 
 interface IUsuarioLogadoContextData {
     nomeDoUsuario: string;
@@ -11,12 +11,20 @@ export const UsuarioLogadoContext = createContext<IUsuarioLogadoContextData>({} 
 
 export const UsuarioLogadoProvider: React.FC = ({}) => {
 
+    const [nome, setNome] = useState('');
+
+    useEffect(() => {
+        setTimeout(() =>  {
+           setNome('Vitor');
+        }, 300);
+    })
+
     const handleLogout = useCallback(() => {
         console.log('logout executou')
     }, [])
 
     return (
-        <UsuarioLogadoContext.Provider value={{ nomeDoUsuario: 'Vitor', logout: handleLogout }}>
+        <UsuarioLogadoContext.Provider value={{ nomeDoUsuario: nome, logout: handleLogout }}>
 
         </UsuarioLogadoContext.Provider>
     );
